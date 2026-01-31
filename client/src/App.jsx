79d855fe87079695e364ab1bab52db0fa4def64e
Amit Sahoo <sahooamit8989@gmail.com>
2026-01-31 07:45:18 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,106 +1,3 @@
-// import { Routes, Route } from "react-router-dom";
-
-// import Navbar from "./componets/Navbar";
-// import Footer from "./componets/Footer";
-// import ScrollToTop from "./componets/ScrollToTop";
-// import { ToastContainer } from 'react-toastify';
-// import "react-toastify/dist/ReactToastify.css";
-
-// import Home from "./pages/Home";
-// import About from "./pages/About";
-// import History from "./componets/History";
-// import VisionMission from "./componets/VisonMission";
-// import Faculty from "./componets/Faculty";
-// import Units from "./componets/Units";
-// import Guruji from "./pages/Guruji";
-// import Gurujis from "./componets/Gurujis";
-// import Milestones from "./componets/Milestones";
-// import Composition from "./componets/Composition";
-// import PhotoGallery from "./componets/PhotoGallery";
-// import Courses from "./pages/Courses";
-// import Regular from "./componets/Regular";
-// import Special from "./componets/Special";
-// import Media from "./pages/Media";
-// import PhotoGraph from "./componets/PhotoGraph";
-// import Video from "./componets/Video";
-// import Contact from "./pages/Contact";
-// import Events from "./pages/Events";
-// import AdminLogin from "./pages/AdminLogin";
-// import AdminDashboard from "./pages/AdminDashboard";
-// import AddPhoto from "./componets/addPhoto";
-// import AddVideo from "./componets/addVideo";
-// import ShowData from "./componets/showData";
-
-// const App = () => {
-//   return (
-//     <>
-//         <ToastContainer />
-//       <ScrollToTop />
-//       <Navbar />
-
-//       <Routes>
-//         <Route path="/" element={<Home />} />
-
-//         {/* ===== ABOUT (NESTED ROUTES) ===== */}
-//         <Route path="/about" element={<About />}>
-//           <Route index element={<History />} />
-//           <Route path="history" element={<History />} />
-//           <Route path="vision-mission" element={<VisionMission />} />
-//           <Route path="faculty" element={<Faculty />} />
-//           <Route path="units" element={<Units />} />
-//         </Route>
-
-//          {/* ===== Guruji (NESTED ROUTES) ===== */}
-//          <Route path="/guruji" element={<Guruji/>}>
-//           <Route index element={<Gurujis/>} />
-//           <Route path="gurujis" element={<Gurujis />} />
-//           <Route path="milestones" element={<Milestones/>} />
-//           <Route path="composition" element={<Composition />} />
-//           <Route path="photoGallery" element={<PhotoGallery/>} />
-//         </Route>
-
-        
-//          {/* ===== course (NESTED ROUTES) ===== */}
-//          <Route path="/course" element={<Courses/>}>
-//           <Route index element={<Regular/>} />
-//           <Route path="regular" element={<Regular />} />
-//           <Route path="special" element={<Special/>} />
-//         </Route>
-
-        
-//          {/* ===== media (NESTED ROUTES) ===== */}
-//          <Route path="/media" element={<Media/>}>
-//           <Route index element={<PhotoGraph/>} />
-//           <Route path="pgotograph" element={<PhotoGraph/>} />
-//           <Route path="video" element={<Video/>} />
-//         </Route>
-         
-//         <Route path="/contact" element={<Contact/>} />
-//         <Route path="/events" element={<Events/>} />
-//         <Route path="/adminlogin" element={<AdminLogin/>} />
-
-//         <Route path="/admin-dashboard" element={<AdminDashboard />}>
-//           <Route index element={<AddPhoto />} />
-//           <Route path="add-photo" element={<AddPhoto />} />
-//           <Route path="add-video" element={<AddVideo />} />
-//           <Route path="show-data" element={<ShowData />} />
-//         </Route>
-
-//       </Routes>
-
-//       <Footer />
-     
-//     </>
-//   );
-// };
-
-// export default App;
-
-
-
-
-
-
 
 
 
@@ -144,9 +41,9 @@ import Special from "./componets/Special";
 import PhotoGraph from "./componets/PhotoGraph";
 import Video from "./componets/Video";
 
-import AddPhoto from "./componets/addPhoto";
-import AddVideo from "./componets/addVideo";
-import ShowData from "./componets/showData";
+import AddPhoto from "./componets/AddPhoto";
+import AddVideo from "./componets/AddVideo";
+import ShowData from "./componets/ShowData";
 import AddEvent from "./componets/AddEvent";
 
 const App = () => {
